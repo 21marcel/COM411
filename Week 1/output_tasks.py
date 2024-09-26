a33a10beb6655842b5a12b16ae74f637f79797c1
MARCEL JNOLEWIS <0jnolm10@solent.ac.uk>
2024-09-26 15:03:03 +0100

    Update with escape characters code

---
+++ Week 1/output_tasks.py
@@ -0,0 +1,17 @@
+# Display message to the screen
+print( "Welcome to COM411")
+print("In week 1 we will learn...")
+print()
+print("... How to use Git and Github")
+print("How to output to the screen")
+print("How to get user input")
+print()
+print("I hope you are enjoying the lesson thus far!")
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a  double quote")
+print("\' Displays a single quote")
+print("\n")
+print("\t\"I am programming!\"")
