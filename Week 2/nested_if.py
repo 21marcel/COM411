3d7f19b352268b4941f47d19740490764bd1eee7
MARCEL JNOLEWIS <0jnolm10@solent.ac.uk>
2024-10-03 14:46:39 +0100

    Multiple nested descions

--- Week 2/nested_if.py
+++ Week 2/nested_if.py
@@ -5,7 +5,28 @@ if cover == "soft":
 bound = input()
 if bound == "yes":
     print("Soft cover, perfect bound books are very popular")
+elif cover == "hard" :
+    print("Books with hard covers can be more expensive")
 else:
     print("Soft covers with coils or stitches are great for short books")
-if cover == "hard":
-    print("Books with hard covers can be more expensive")
+
+
+response= input("Where should I look?")
+if response == "in the bedroom":
+    bedroom = input("Where in the bedroom should I look?")
+    if bedroom == "under the bed":
+        print("Found some shoes but no phone")
+    else:
+        print("Found some mess but no phone")
+elif response == "in the bathroom":
+    bathroom = input("Where in the bathroom should I look?")
+    if bathroom == "in the sink":
+        print("Found some hair but no phone")
+    else:
+        print("Found some bathroom stuff but no phone")
+elif response == "in the living room":
+    living_room = input("Where in the living room should I look?")
+    if living_room == "on the table":
+        print(" I found my phone")
+    else:
+        print("Found some stuff but no phone")
