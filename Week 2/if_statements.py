497500cd89584b86d2cd255d04c2741b5baebbf4
MARCEL JNOLEWIS <0jnolm10@solent.ac.uk>
2024-10-03 13:16:14 +0100

    If and else statments

--- Week 2/if_statements.py
+++ Week 2/if_statements.py
@@ -4,3 +4,10 @@ book = input()
 if book == "adventure":
     print("I like adventure books!")
 print ("Finished reading book!")
+
+print("Please enter activity to be performed")
+activity = input()
+if activity == "calculate":
+    print("Performing calculations...")
+else:print("Performing activity...")
+print("Activity completed!")
