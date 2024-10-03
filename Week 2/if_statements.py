4a98f8d41f0fd605ae86b290f99f116b3bdad583
MARCEL JNOLEWIS <0jnolm10@solent.ac.uk>
2024-10-03 13:28:31 +0100

    Tested if and elseif statements

--- Week 2/if_statements.py
+++ Week 2/if_statements.py
@@ -1,9 +1,9 @@
 print("What type of book is this?")
 book = input()
-
 if book == "adventure":
     print("I like adventure books!")
 print ("Finished reading book!")
+
 
 print("Please enter activity to be performed")
 activity = input()
@@ -11,3 +11,14 @@ if activity == "calculate":
     print("Performing calculations...")
 else:print("Performing activity...")
 print("Activity completed!")
+
+print("Which direction should I go?")
+direction = input(). lower()
+if direction == "up":
+    print("I am moving in an upward direction!")
+elif direction == "down":
+    print("I am moving in an downward direction!")
+elif direction == "left":
+    print("I am moving in an leftward direction!")
+elif direction == "right":
+    print("I am moving in an rightward direction!")
