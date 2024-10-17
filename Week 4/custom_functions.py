3b522ab21daab54675a33f500e277d674a2d5531
MARCEL JNOLEWIS <0jnolm10@solent.ac.uk>
2024-10-17 14:54:25 +0100

    Added code to demonstrate the use of user definded function with paremeter

--- Week 4/custom_functions.py
+++ Week 4/custom_functions.py
@@ -14,3 +14,17 @@ def identify():
         print ("Ah we will be fine")
 
 identify()
+
+def escape_by(plan):
+    if plan == "Jumping over":
+        print("We cannot escape that way!The boulder is too big!")
+    elif plan == "Running around":
+        print("We cannot escape that way!The boulder is moving too fast!")
+    elif plan == "Cross bridge ahead":
+        print("We cannot escape that way!The boulder is in the way!")
+
+escape_by("Jumping over")
+escape_by("Running around")
+escape_by("Cross bridge ahead")
+
+
