25b7568b8b879cd45132be9034f1fde5a7b78a53
MARCEL JNOLEWIS <0jnolm10@solent.ac.uk>
2024-10-17 13:36:33 +0100

    Added code to demonstrate the use of built in functions :abs,range chr

--- Week 4/built_in_functions.py
+++ Week 4/built_in_functions.py
@@ -3,5 +3,15 @@ print("Please enter a standard character")
 character = input()
 
 ascii=ord(character)
-
 print(f"The ASCII value of {character} is {ascii}")
+print("Program Ended!")
+
+print("Program Started!")
+print("Please enter an ASCII code:")
+code = int(input())
+if code in range(32,127):
+    ascii=chr(code)
+    print(f"The character represented by the ASCII code {code} is {ascii}")
+else:
+    print("Please enter a value between 32 and 126")
+print("Program Ended!")
