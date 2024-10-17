4005bc0d0e9053fbaa160bc18dfefd371e1c4cc9
MARCEL JNOLEWIS <0jnolm10@solent.ac.uk>
2024-10-17 14:25:17 +0100

    Added code to demonstrate the use of user definded function with a nested descion

--- Week 4/custom_functions.py
+++ Week 4/custom_functions.py
@@ -4,3 +4,13 @@ def listen():
     print(f"That was a loud {sound}!")
 
 listen()
+
+def identify():
+    print("What lies before us?")
+    lies = input()
+    if lies == ("A large boulder"):
+        print("It's time to run!")
+    else:
+        print ("Ah we will be fine")
+
+identify()
