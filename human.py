59aa764240387ca931a5b7b5491e1598b2745b34
MARCEL JNOLEWIS <0jnolm10@solent.ac.uk>
2024-11-21 14:41:57 +0000

    Added code to demonstrate human classes

---
+++ human.py
@@ -0,0 +1,7 @@
+class Human:
+    MAX_ENERGY = 100
+
+    def __init__(self):
+        self.name = "Human"
+        self.age = 0
+        self.energy = Human.MAX_ENERGY
---
+++ main.py
@@ -0,0 +1,5 @@
+from human import Human
+
+h1 = Human()
+
+print(f"Hi, I am {h1.name} and I am {h1.age} years old")
